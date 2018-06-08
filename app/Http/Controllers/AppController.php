5e46c24757c56d52a2066bfaeb6fa183fa9ce2db
Vlad Smith <stuf.developer@gmail.com>
2018-06-08 13:13:02 +0300

    styles

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    //
+    function __construct()
+    {
+
+    }
+
+    /**
+     * Метод главной страницы веб-приложения
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    function index()
+    {
+        echo "MVC";
+        return view('index');
+    }
+}
