feed649058cfb15c8080d9e658ef4dd6338467a8
Vlad Smith <stuf.developer@gmail.com>
2018-06-08 13:12:45 +0300

    styles

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="{!! asset('css/app.css') !!}" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <section class="wrapper main-page__wrapper">
+
+    </section>
+</body>
+</html>
